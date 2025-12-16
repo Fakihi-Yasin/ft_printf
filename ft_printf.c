83fd3b5255830c14e6214a7561c2d35a379e7d61
Fakihi-Yasin <yasinfakihi21@gmail.com>
2025-12-16 17:06:24 +0100

    initial commit

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: yafakihi <yafakihi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/12 10:41:48 by yafakihi          #+#    #+#             */
-/*   Updated: 2025/12/16 16:10:32 by yafakihi         ###   ########.fr       */
+/*   Updated: 2025/12/16 16:59:15 by yafakihi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,16 @@ static int	handle_format(const char specifier, va_list args)
 }
 int ft_printf(const char *format, ...)
 {
-    if(for == )
+    va_list ptr;
+    int     count;
+    if(format == NULL )
+        return (0);
+    count = 0;
+    while (*format)
+    {
+        
+    }
+    
 }
 int main(void)
 {
