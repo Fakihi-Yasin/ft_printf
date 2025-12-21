544a7c17d2f66fb2abeb578f5699d489bd92e8ed
Fakihi-Yasin <yasinfakihi21@gmail.com>
2025-12-21 01:20:51 +0100

    fix norminate

--- ft_printf.c
+++ ft_printf.c
@@ -12,6 +12,17 @@
 
 #include "ft_printf.h"
 
+static int	ft_putnbr_unsigned(unsigned int n)
+{
+	int	count;
+
+	count = 0;
+	if (n >= 10)
+		count += ft_putnbr_unsigned(n / 10);
+	count += ft_putchar((n % 10) + '0');
+	return (count);
+}
+
 int	printformat(char specifier, va_list ptr)
 {
 	int	count;
@@ -21,73 +32,75 @@ int	printformat(char specifier, va_list ptr)
 		count += ft_putchar(va_arg(ptr, int));
 	else if (specifier == 's')
 		count += ft_putstr(va_arg(ptr, char *));
-	else if(specifier == 'd' || specifier == 'i')
+	else if (specifier == 'd' || specifier == 'i')
 		count += ft_putnbr(va_arg(ptr, int));
-	else if(specifier == 'x')
+	else if (specifier == 'x')
 		count += ft_puthex(va_arg(ptr, unsigned int), 0);
-	else if(specifier == 'X')
+	else if (specifier == 'X')
 		count += ft_puthex(va_arg(ptr, unsigned int), 1);
-	else if(specifier == 'u')
+	else if (specifier == 'u')
 		count += ft_putnbr_unsigned(va_arg(ptr, unsigned int));
-	else if(specifier == 'p')
+	else if (specifier == 'p')
 		count += ft_putptr(va_arg(ptr, void *));
-	else if(specifier == '%')
+	else if (specifier == '%')
 		count += ft_putchar('%');
-	return count;
+	return (count);
 }
 
 int	ft_printf(const char *format, ...)
 {
 	va_list	ptr;
-	va_start(ptr, format);
 	int		count;
-	int     i;
-	i = 0;
+	int		i;
 
-	if (format == NULL)
+	if (!format)
 		return (0);
+	va_start(ptr, format);
+	i = 0;
 	count = 0;
-  	while (format[i])
+	while (format[i])
 	{
-		if (format[i] == '%' && format[i + 1]){
-			i++;
-			count += printformat(format[i], ptr);
+		if (format[i] == '%' && format[i + 1])
+		{
+			count += printformat(format[i++], ptr);
 		}
-		else if(format[i] == '%' && !format[i + 1]){
-			break;
+		else if (format[i] == '%' && !format[i + 1])
+		{
+			break ;
 		}
-		else{
+		else
 			count += ft_putchar(format[i]);
-		}
 		i++;
 	}
 	va_end(ptr);
 	return (count);
 }
-int main() {
-    int x = 42;
 
-    ft_printf("=== Test 1: All conversions ===\n");
-    ft_printf("char: %c\n", 'A');
-    ft_printf("string: %s\n", "Hello");
-    ft_printf("pointer: %p\n", &x);
-    ft_printf("decimal: %d\n", -42);
-    ft_printf("integer: %i\n", 123);
-    ft_printf("unsigned: %u\n", 4294967295u);
-    ft_printf("hex lower: %x\n", 255);
-    ft_printf("hex upper: %X\n", 255);
-    ft_printf("percent: %%\n");
+// int	main(void)
+// {
+// 	int x = 42;
 
-    ft_printf("\n=== Test 2: Multiple variables ===\n");
-    ft_printf("Mix: %c %s %d %x %p\n", 'Z', "test", 42, 255, &x);
+// 	ft_printf("=== Test 1: All conversions ===\n");
+// 	ft_printf("char: %c\n", 'A');
+// 	ft_printf("string: %s\n", "Hello");
+// 	ft_printf("pointer: %p\n", &x);
+// 	ft_printf("decimal: %d\n", -42);
+// 	ft_printf("integer: %i\n", 123);
+// 	ft_printf("unsigned: %u\n", 4294967295u);
+// 	ft_printf("hex lower: %x\n", 255);
+// 	ft_printf("hex upper: %X\n", 255);
+// 	ft_printf("percent: %%\n");
 
-    ft_printf("\n=== Test 3: Five variables ===\n");
-    ft_printf("v1=%d, v2=%d, v3=%d, v4=%d, v5=%d\n", 10, 20, 30, 40, 50);
+// 	ft_printf("\n=== Test 2: Multiple variables ===\n");
+// 	ft_printf("Mix: %c %s %d %x %p\n", 'Z', "test", 42, 255, &x);
 
-    ft_printf("\n=== Test 4: Edge cases ===\n");
-    ft_printf("NULL string: %s\n", NULL);
-    ft_printf("Zero: %d\n", 0);
-    ft_printf("Negative: %d\n", -2147483648);
+// 	ft_printf("\n=== Test 3: Five variables ===\n");
+// 	ft_printf("v1=%d, v2=%d, v3=%d, v4=%d, v5=%d\n", 10, 20, 30, 40, 50);
 
-    return (0);
-}
+// 	ft_printf("\n=== Test 4: Edge cases ===\n");
+// 	ft_printf("NULL string: %s\n", NULL);
+// 	ft_printf("Zero: %d\n", 0);
+// 	ft_printf("Negative: %d\n", -2147483648);
+
+// 	return (0);
+// }
--- ft_printf_utils.c
+++ ft_printf_utils.c
@@ -13,72 +13,71 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int ft_putchar(char c) {
-    return write(1, &c, 1);
-}
-
-int ft_putstr(char *s) {
-    int count = 0;
-    if (!s)
-        return ft_putstr("(null)");
-    while (*s) {
-        count += ft_putchar(*s);
-        s++;
-    }
-    return count;
-}
-
-int ft_putnbr(int n) {
-    int count = 0;
-    long num = n;
-    
-    if (num < 0) {
-        count += ft_putchar('-');
-        num = -num;
-    }
-    if (num >= 10)
-        count += ft_putnbr(num / 10);
-    count += ft_putchar((num % 10) + '0');
-    return count;
-}
-
-int ft_putnbr_unsigned(unsigned int n) {
-    int count = 0;
-    
-    if (n >= 10)
-        count += ft_putnbr_unsigned(n / 10);
-    count += ft_putchar((n % 10) + '0');
-    return count;
-}
-int ft_puthex(unsigned long n, int uppercase)
+int	ft_putchar(char c)
 {
-    char *hex_digits;
-    int count;
-
-    count = 0;
-    if (uppercase)
-        hex_digits = "0123456789ABCDEF";
-    else
-        hex_digits = "0123456789abcdef";
-
-    if (n >= 16)
-        count += ft_puthex(n / 16, uppercase);
-
-    count += ft_putchar(hex_digits[n % 16]);
-    return (count);
+	return (write(1, &c, 1));
 }
 
-int ft_putptr(void *ptr) {
-    int count = 0;
-    unsigned long addr = (unsigned long)ptr;
-    
-    count += ft_putstr("0x");
-    count += ft_puthex(addr, 0);
-    return count;
+int	ft_putstr(char *s)
+{
+	int	count;
+
+	count = 0;
+	if (!s)
+		return (ft_putstr("(null)"));
+	while (*s)
+	{
+		count += ft_putchar(*s);
+		s++;
+	}
+	return (count);
 }
 
+int	ft_putnbr(int n)
+{
+	int		count;
+	long	num;
 
+	count = 0;
+	num = n;
+	if (num < 0)
+	{
+		count += ft_putchar('-');
+		num = -num;
+	}
+	if (num >= 10)
+		count += ft_putnbr(num / 10);
+	count += ft_putchar((num % 10) + '0');
+	return (count);
+}
 
+int	ft_puthex(unsigned long n, int uppercase)
+{
+	char	*hex_digits;
+	int		count;
+
+	count = 0;
+	if (uppercase)
+		hex_digits = "0123456789ABCDEF";
+	else
+		hex_digits = "0123456789abcdef";
+	if (n >= 16)
+		count += ft_puthex(n / 16, uppercase);
+	count += ft_putchar(hex_digits[n % 16]);
+	return (count);
+}
+
+int	ft_putptr(void *ptr)
+{
+	int				count;
+	unsigned long	addr;
+
+	count = 0;
+	addr = (unsigned long)ptr;
+	count += ft_putstr("0x");
+	count += ft_puthex(addr, 0);
+	return (count);
+}
 
 // int main ()
 // {
